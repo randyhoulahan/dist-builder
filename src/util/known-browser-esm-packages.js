ca39ad72089166b63df7478357922683100160aa
Randy Houlahan <randy.houlahan@cbd.int>
2020-09-30 21:34:48 -0400

    fix major libs

--- src/util/known-browser-esm-packages.js
+++ src/util/known-browser-esm-packages.js
@@ -1,7 +1,7 @@
 const defaultCdn = 'https://unpkg.com/'
 
 export const knownBrowserEsmPackages = {
-  vue        : `${defaultCdn}/vue@~2.6/dist/vue.esm.browser.min.js`,
-  'vue-i18n' : `${defaultCdn}/vue-i18n@~8.21/dist/vue-i18n.esm.browser.min.js`,
-  ky         : `${defaultCdn}/ky@~0.23`
+  vue           : `${defaultCdn}/vue@2.6.11/dist/vue.esm.browser.min.js`,
+  'vue-i18n'    : `${defaultCdn}/vue-i18n@8.21.1/dist/vue-i18n.esm.browser.min.js`,
+  'ky-universal': `${defaultCdn}/ky@0.24.0`
 }
